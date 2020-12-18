f552e8b2e9cda0e61015ee80267ae8c1b47e8c2f
sami2020pro <samprogram2007@gmail.com>
2020-12-18 21:29:10 +0330

    Added sections and source

---
+++ sections/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+        println!("Hello World")
+}
+
+fn main() {
+        println!("сьчцйка")
+}
+
+fn main() {
+        println!("\u{f001}")
+}
+
+fn main() {
+        println!("\x1b[35mHello World")
+}
+
+fn main() {
+        println!("{}", "Hello World")
+}
