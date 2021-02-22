d7e70d56f18d539d08406e0b68bba7f900c22938
sami2020pro <samprogram2007@gmail.com>
2021-02-22 19:34:38 +0330

    update: add new codes

--- sections/section-eight/src/main.rs
+++ sections/section-eight/src/main.rs
@@ -8,3 +8,19 @@ fn main() {
 fn calculate_length(s: &String) -> usize {
     s.len()
 }
+
+fn main() {
+    let x: usize = 1; // usize can hold to zero or more than zero
+    let y: usize = 0; // usize can hold to zero or more than zero
+    let z: isize = -10; // isize can hold to zero or more than zero OR less than zero
+
+    println!("x {} | y {} | z {}", x, y, z);
+}
+
+fn main() {
+    let x: usize = 1; // usize can hold to zero or more than zero
+    let y: usize = 0; // usize can hold to zero or more than zero
+    let z: usize = -10; // Error, We need ISIZE !!!
+
+    println!("x {} | y {} | z {}", x, y, z);
+}
